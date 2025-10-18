659ae02d81b1da4cf7986426f94a4aac83e47244
Juanjo Castro Rodriguez <juanjocastro0729@gmail.com>
2025-10-18 00:03:19 -0500

    suffle is working

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -27,8 +27,20 @@ onMounted(async () => {
    })
   })
 
+  answers.value = suffle(answers.value)
+  //console.log(question.value)
 })
 
+
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--){
+    const j = Math.floor(Math.random() * (i + 1));
+    //swap
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array
+}
 </script>
 
 <template>
