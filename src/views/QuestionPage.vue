228a3cd261af2d2a799df2349f314443020ff945
Juanjo Castro Rodriguez <juanjocastro0729@gmail.com>
2025-10-22 23:52:14 -0500

    difficulty added

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -3,6 +3,7 @@ import { onMounted, ref } from 'vue';
 import useAPI from '@/composables/useAPI';
 import { useRoute } from 'vue-router';
 import BaseTitle from '@/components/BaseTitle.vue';
+import DifficultyChip from '@/components/DifficultyChip.vue';
 
 
 const api = useAPI()
@@ -58,7 +59,7 @@ const shuffle = (array) => {
 
       </div>
     </div>
-
+    <DifficultyChip :difficulty="question.difficulty"></DifficultyChip>
 
   </div>
   <div v-else class="">
